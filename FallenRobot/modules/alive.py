7a73a7909a768fb8a38c202099bbf53513ce1aed
Mohankumarroshini <89738222+Mohankumarroshini@users.noreply.github.com>
2022-07-08 14:43:29 +0530

    Update alive.py

--- FallenRobot/modules/alive.py
+++ FallenRobot/modules/alive.py
@@ -11,15 +11,31 @@ from FallenRobot import telethn as tbot, SUPPORT_CHAT, OWNER_USERNAME, dispatche
 
 
 PHOTO = [
-    "https://telegra.ph/file/6a21ea6677342f43b363e.jpg",
-    "https://telegra.ph/file/40eb1ed850cdea274693e.jpg",
+       "https://telegra.ph/file/0c63efafefa0a214d8df8.jpg".
+
+"https://telegra.ph/file/0a020df4a384b96462559.jpg",
+
+"https://telegra.ph/file/4b5abc8970bc2bc282040.jpg",
+
+"https://telegra.ph/file/6a420caf4e279ed68b7ea.jpg",
+
+"https://telegra.ph/file/3b155e569ee34f6be90c2.jpg",
+
+"https://telegra.ph/file/f75ebb454c6c8993fef9e.jpg",
+
+"https://telegra.ph/file/a67dc5bce8e4cc4552064.jpg",
+
+"https://telegra.ph/file/f24142a097b9a877cad42.jpg",
+
+"https://telegra.ph/file/339d6bf3f0b05f3b13123.jpg",
+
 ]
 
 
 @register(pattern=("/alive"))
 async def awake(event):
     TEXT = f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ {dispatcher.bot.first_name}**\n━━━━━━━━━━━━━━━━━━━\n\n"
-    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [𝝙𝗡𝗢𝗡𝗬𝗠𝗢𝗨𝗦](https://t.me/{OWNER_USERNAME})** \n\n"
+    TEXT += f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [எம்.எஸ்.டி🇮🇳](https://t.me/{OWNER_USERNAME})** \n\n"
     TEXT += f"» **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
     TEXT += f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
     TEXT += f"» **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n━━━━━━━━━━━━━━━━━\n\n"
